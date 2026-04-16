596f3e72f70ca52609b9228c9eda46191b27864e
criadosenai244 <criadosenai244@gmail.com>
2026-04-16 16:06:11 -0300

    aula 08 introdução java script

---
+++ JAVA SCRIPT/AULA 1 - INTRODUÇÃO/scripts/aula.js
@@ -0,0 +1,28 @@
+// RESPOSTAS  NO CONSOLE
+console.log("olá mundo!!");
+
+// CAIXA DE DIALOGO
+alert("CHAMMAAAAAAAA!!!!!");
+prompt ("QUAL SEU NOME??")
+confirm("VOCE FARIA UM PIX PRO SEU COLEGA ??");
+//  acessar documento, buscar o elemento com o id selecionado
+// inner= dentro
+
+document.getElementById("resposta") . innerText= "<h1> que legal</h1>";
+document.getElementById("resposta"). innerHTML="<h1> que legal</h1> ";
+// string
+var nome = "vitor";
+// boolean - logico (verdadeiro- true , 'falso' - false)
+var ligado = true;
+// number - numero (80)
+// *obs para numeros quebrados - float()
+// *obs para numeros inteiros - int()
+var idade = 23;
+var dinheiro = 108,50
+//  array - lista , vetor
+var listaCompras = []
+console.log(typeof(nome));
+console.log(typeof(ligado));
+console.log(typeof(idade));
+console.log(typeof(dinheiro))
+console.log(typeof(listaCompras))
---
+++ JAVA SCRIPT/AULA 1 - INTRODUÇÃO/scripts/usuario.js
@@ -0,0 +1,4 @@
+var nome = prompt (" Qual é o seu nome? ");
+
+// document. getElementById ("resposta").innerHTML="<hi>"+nome+"</h1>"
+document.getElementById("resposta"). innerHTML = `<h1> ${nome}</h1>`;
